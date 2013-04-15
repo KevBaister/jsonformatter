9d1c5464fe98d0b4f40c0c98f33c50925dfef6bc
Kev Baister <kevbaister+code@gmail.com>
2013-04-15 20:52:21 +0100

    Implemented basic JSON formatting

---
+++ src/jsonformatter/Content/scripts/app.js
@@ -0,0 +1,47 @@
+﻿var formatter = (function () {
+
+    var $editor;
+
+    var init = function() {
+        $editor = $('#raw-json');
+
+        $('#format').click(onFormatClicked);
+        $('#clear').click(onClearClicked);
+    };
+
+    var onFormatClicked = function () {
+
+        var jsonData = $editor.val();
+        
+        if (jsonData !== '') {
+            var spacing = $('#spacing').val();
+            
+            if ($.isNumeric(spacing)) {
+                spacing = parseInt(spacing, 10);
+            } else if(spacing === 'tab') {
+                spacing = '\t';
+            }
+
+            try {
+                var parsedJson = JSON.parse($editor.val());
+                $editor.val(JSON.stringify(parsedJson, null, spacing));
+            } catch(e) {
+                alert('The JSON you have entered does not appear to be valid.');
+            } 
+        }
+
+    };
+
+    var onClearClicked = function() {
+        $editor.val('');
+    };
+
+    return {
+        init: init
+    };
+
+}());
+
+$(function() {
+    formatter.init();
+});
